e5c259a9cc76b35d3721a251bfbb5fcd4facec5b
C. Allwardt <craig.allwardt@pnnl.gov>
2021-07-09 12:26:01 -0700

    Added networking functions

--- volttron/utils/network.py
+++ volttron/utils/network.py
@@ -36,12 +36,19 @@
 # under Contract DE-AC05-76RL01830
 # }}}
 
-
+import logging
+import os
+import sys
 import re
+import traceback
+
+from volttron.utils import ClientContext as cc
+
+_log = logging.getLogger(__name__)
 
 
 def is_ip_private(vip_address):
-    """ Determines if the passed vip_address is a private ip address or not.
+    """Determines if the passed vip_address is a private ip address or not.
 
     :param vip_address: A valid ip address.
     :return: True if an internal ip address.
@@ -55,14 +62,61 @@ def is_ip_private(vip_address):
     priv_20 = re.compile("^192\.168\.\d{1,3}.\d{1,3}$")
     priv_16 = re.compile("^172.(1[6-9]|2[0-9]|3[0-1]).[0-9]{1,3}.[0-9]{1,3}$")
 
-    return priv_lo.match(ip) is not None or priv_24.match(
-        ip) is not None or priv_20.match(ip) is not None or priv_16.match(
-        ip) is not None
+    return (
+        priv_lo.match(ip) is not None
+        or priv_24.match(ip) is not None
+        or priv_20.match(ip) is not None
+        or priv_16.match(ip) is not None
+    )
 
 
 def get_hostname():
-    with open('/etc/hostname') as fp:
+    with open("/etc/hostname") as fp:
         hostname = fp.read().strip()
 
-    assert hostname
+    if not hostname:
+        raise ValueError("/etc/hostname file not found!")
     return hostname
+
+
+def get_address(verify_listening=False):
+    """Return the VIP address of the platform
+    If the VOLTTRON_VIP_ADDR environment variable is set, it is used to connect to.
+    Otherwise, it is derived from get_home()."""
+    address = os.environ.get("VOLTTRON_VIP_ADDR")
+    if not address:
+        # Connect via virtual unix socket if linux platform (mac doesn't have @ in it)
+        abstract = "@" if sys.platform.startswith("linux") else ""
+        address = "ipc://%s%s/run/vip.socket" % (abstract, cc.get_volttron_home())
+
+    import zmq.green as zmqgreen
+    import zmq
+
+    # The following block checks to make sure that we can
+    # connect to the zmq based upon the ipc address.
+    #
+    # The zmq.sock.bind() will raise an error because the
+    # address is already bound (therefore volttron is running there)
+    sock = None
+    try:
+        # TODO: We should not just do the connection test when verfiy_listening is True but always
+        # Though we leave this here because we have backward compatible unit tests that require
+        # the get_address to not have somethiing bound to the address.
+        if verify_listening:
+            ctx = zmqgreen.Context.instance()
+            sock = ctx.socket(zmq.PUB)  # or SUB - does not make any difference
+            sock.bind(address)
+            raise ValueError(
+                "Unable to connect to vip address "
+                f"make sure VOLTTRON_HOME: {cc.get_volttron_home()} "
+                "is set properly"
+            )
+    except zmq.error.ZMQError as e:
+        _log.error(f"Zmq error was {e}\n{traceback.format_exc()}")
+    finally:
+        try:
+            sock.close()
+        except AttributeError as e:  # Raised when sock is None type
+            pass
+
+    return address
