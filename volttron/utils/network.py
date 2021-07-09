0d00ce44d12347997052173f48e4eb6a75c6a90a
C. Allwardt <craig.allwardt@pnnl.gov>
2021-07-09 12:30:22 -0700

    Fixed network re warnings

--- volttron/utils/network.py
+++ volttron/utils/network.py
@@ -57,9 +57,9 @@ def is_ip_private(vip_address):
 
     # https://en.wikipedia.org/wiki/Private_network
 
-    priv_lo = re.compile("^127\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
-    priv_24 = re.compile("^10\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
-    priv_20 = re.compile("^192\.168\.\d{1,3}.\d{1,3}$")
+    priv_lo = re.compile("^127.\d{1,3}.\d{1,3}.\d{1,3}$")
+    priv_24 = re.compile("^10.\d{1,3}.\d{1,3}.\d{1,3}$")
+    priv_20 = re.compile("^192.168.\d{1,3}.\d{1,3}$")
     priv_16 = re.compile("^172.(1[6-9]|2[0-9]|3[0-1]).[0-9]{1,3}.[0-9]{1,3}$")
 
     return (
