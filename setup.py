e6cf8db9d18b3dbf90eabd2bdc8daf8a23e56720
C. Allwardt <craig.allwardt@pnnl.gov>
2021-11-24 09:22:18 -0800

    Updated setup.py removing cruf.

--- setup.py
+++ setup.py
@@ -7,13 +7,7 @@ Modified by Madoshakalaka@Github (dependency links added)
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
@@ -76,36 +70,5 @@ setup(
     # see https://python-packaging.readthedocs.io/en/latest/dependencies.html#packages-not-on-pypi
     #
     dependency_links=[],
-    # If using Python 2.6 or earlier, then these have to be included in
-    # MANIFEST.in as well.
-    # package_data={"sample": ["package_data.dat"]},  # Optional
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    #
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[("my_data", ["data/data_file"])],  # Optional
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    # entry_points={"console_scripts": ["sample=sample:main"]},  # Optional
-    # List additional URLs that are relevant to your project as a dict.
-    #
-    # This field corresponds to the "Project-URL" metadata fields:
-    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
-    #
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
-    project_urls={  # Optional
-        "Bug Reports": "https://github.com/VOLTTRON/volttron-utils/issues",
-        #        "Funding": "https://donate.pypi.org",
-        #        "Say Thanks!": "http://saythanks.io/to/example",
-        "Source": "https://github.com/VOLTTRON/volttron-utils/",
-    },
+
 )
